3ad1de95642bf8891f39d4f165537e0b89d01fa9
rishabhtak <rishabhtak93@gmail.com>
2023-07-04 01:11:11 +0530

    added navbar

---
+++ hotel-frontend/src/app/components/navbar/Navbar.js
@@ -0,0 +1,40 @@
+'use client'
+import { useState } from "react";
+import Link from "next/link"
+import styles from "../navbar/navbar.module.css"
+import { BiMenu, BiX } from 'react-icons/bi';
+
+function Navbar() {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+    const closeSidebar = () => setSidebarOpen(false);
+
+
+    return (
+        <>
+            <nav className={`${styles.navbar} ${sidebarOpen ? styles.active : ""}`} >
+                <div className={styles.navbarList}>
+                    <BiMenu color="white" size={30} onClick={toggleSidebar} className={`${sidebarOpen} ? ${styles.sidebarOpen} : "" `} />
+                    <span className={`${styles.logo} ${styles.navLogo}`}><Link href="/">CodingLab</Link></span>
+                    <div className={styles.menu}>
+                        <div className={styles.logo_toggle}>
+                            <span className={styles.logo}><Link href="/">CodingLab</Link></span>
+                            <BiX color="white" size={30} onClick={closeSidebar} style={{ cursor: 'pointer' }} />
+                        </div>
+                        <ul className={styles.nav_links}>
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/about">About</Link></li>
+                            <li><Link href="/">Portfolio</Link></li>
+                            <li><Link href="/">Services</Link></li>
+                            <li><Link href="/">Contact</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </>
+    )
+}
+
+export default Navbar
---
+++ hotel-frontend/src/app/page.js
@@ -0,0 +1,38 @@
+
+export default function Home() {
+  return (
+    <div>
+      <div>this is a home</div>
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+      <div>this is a home</div>
+
+
+
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+
+      <div>this is a home</div>
+
+
+
+      <div>this is a home</div>
+
+
+    </div>
+  )
+}
