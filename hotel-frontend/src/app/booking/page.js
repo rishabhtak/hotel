3d61ebdd1dba7f2ef3020fb8a922ff79b9aae343
rishabhtak <rishabhtak93@gmail.com>
2023-08-08 20:08:28 +0530

    added check in calender

--- hotel-frontend/src/app/booking/page.js
+++ hotel-frontend/src/app/booking/page.js
@@ -1,42 +1,41 @@
 'use client'
 import { useState } from 'react'
 import Breadcrumb from '@/components/layout/Breadcrumb'
-import { format, addDays, add } from 'date-fns'
+import Datepicker from "react-tailwindcss-datepicker";
+
 
 
 export default function Booking() {
-    const [startDate, setStartDate] = useState(format(new Date(), 'yyyy-MM-dd'));
-    const [endDate, setEndDate] = useState(format(addDays(new Date(), 1), 'yyyy-MM-dd'));
 
+    const [value, setValue] = useState({
+        startDate: null,
+        endDate: null
+    });
 
-    const onStartDate = (e) => {
-        console.log("end date", e.target.value)
-        setStartDate(e.target.value)
-
+    const handleValueChange = (newValue) => {
+        console.log("newValue:", newValue);
+        setValue(newValue);
     }
-
-    const onEndDate = (e) => {
-        console.log("end date", e.target.value)
-        setEndDate(e.target.value);
-    }
-
-    console.log(format(addDays(new Date(endDate), 365), 'yyyy-MM-dd'));
     return (
         <>
             <Breadcrumb img="/booking.jpg" pageName="booking" />
             <div className="flex flex-col items-center justify-center mx-auto">
-                <div className='w-full md:w-[25rem] lg:w-[50rem] py-10 px-5 -mt-1 bg-white rounded-lg shadow-lg dark:bg-gray-800 absolute'>
-                    <div className="grid lg:grid-cols-3 gap-10">
+                <div className='w-full md:w-[40rem] lg:w-[50rem] py-5 lg:py-10 px-5 -mt-1 bg-white rounded-lg shadow-lg dark:bg-gray-800 absolute'>
+                    <div className="grid md:grid-cols-2 gap-10">
                         <div className='flex flex-col'>
-                            <label htmlFor="fname">Check-in</label>
-                            <input className='border px-4 py-2' type="date" id="from" name="from" value={startDate} min={startDate} max={format(addDays(new Date(), 365), 'yyyy-MM-dd')} onChange={onStartDate} />
-                        </div>
+                            <label htmlFor="fname" className='ml-2 font-bold'>Check-in / Check-out</label>
+                            <Datepicker
+                                inputClassName="w-full rounded-lg font-normal border py-2 px-2"
+                                value={value}
+                                onChange={handleValueChange}
+                                readOnly={true}
+                                minDate={new Date()}
+                            />
 
-                        <div className='flex flex-col'>
-                            <label htmlFor="fname">Check-out</label>
-                            <input className='border px-4 py-2' type="date" id="to" name="to" min={endDate} max={format(addDays(new Date(), 365), 'yyyy-MM-dd')} value={endDate} onChange={onEndDate} />
                         </div>
-                        <button className='border bg-blue-gray-100 tracking-wide lg:mt-6'>Check Availability</button>
+                        <button className="w-full px-6 py-2.5 text-lg font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-6 focus:outline-none bg-button-color rounded-lg focus:ring focus:ring-gray-300 focus:ring-opacity-80">
+                            Check Availability
+                        </button>
                     </div>
                 </div>
             </div>
