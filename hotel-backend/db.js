fe74429c3710d6be529637e512afc9f9688b079b
rishabhtak <rishabhtak93@gmail.com>
2024-01-20 21:40:18 +0530

    env update

--- hotel-backend/db.js
+++ hotel-backend/db.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const mongoURI = "mongodb://0.0.0.0:27017/hotel-backend?directConnection=true&tls=false&readPreference=primary";
+const mongoURI = process.env.MONGODB_SECRET;
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI)
-        .then(() => console.log('Mongoose Connected!'));
-}
-
+  mongoose.connect(mongoURI).then(() => console.log("Mongoose Connected!"));
+};
 
 module.exports = connectToMongo;
