40ed5e5876c08b8267dc5669d564aed2155a71d7
rishabhtak <rishabhtak93@gmail.com>
2024-01-22 18:22:13 +0530

    server testing

--- hotel-backend/index.js
+++ hotel-backend/index.js
@@ -1,37 +1,34 @@
 const connectToMongo = require("./db");
-const express = require('express');
-const cors = require('cors')
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 connectToMongo();
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 //middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', express.static(__dirname));
+// app.use("/", express.static(__dirname));
 
 //available routes
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/booking', require('./routes/booking'));
-app.use('/api/admin', require('./routes/admin'));
-app.use('/api/room', require('./routes/room'));
-app.use('/api/roomdetail', require('./routes/roomdetail'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/booking", require("./routes/booking"));
+app.use("/api/admin", require("./routes/admin"));
+app.use("/api/room", require("./routes/room"));
+app.use("/api/roomdetail", require("./routes/roomdetail"));
 
 
-
-app.get('/', (req, res) => {
-    res.send('Server is running')
-})
-
-
-
-
+app.get("/", (req, res) => {
+  // res.send("Server is running");
+  app.use(express.static(path.resolve(__dirname, "admin", "build")));
+  res.sendFile(path.resolve(__dirname, "admin", "build", "index.html"));
+});
 
 app.listen(port, () => {
-    console.log(`Hotel-backend app listening on port http://localhost:${port}`)
-})
+  console.log(`Hotel-backend app listening on port http://localhost:${port}`);
+});
