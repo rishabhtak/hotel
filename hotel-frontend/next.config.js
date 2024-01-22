e79407cd1274ef944ff750361eb2707aa11d2182
rishabhtak <rishabhtak93@gmail.com>
2024-01-22 21:35:05 +0530

    pm2 uninstall and nodemon install

--- hotel-frontend/next.config.js
+++ hotel-frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ["hotel-backend-h62d.onrender.com"],
   },
   reactStrictMode: true,
 };
