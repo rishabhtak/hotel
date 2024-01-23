dfbc362e5d2456d67c6f73438b9b8daad12580aa
rishabhtak <rishabhtak93@gmail.com>
2024-01-23 17:31:08 +0530

    next config

--- hotel-frontend/next.config.js
+++ hotel-frontend/next.config.js
@@ -1,21 +1,11 @@
-/** @type {import('next').NextConfig} 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["hotel-backend-jrld.onrender.com"],
+    domains: ["https://hotel-backend-jrld.onrender.com"],
   },
   reactStrictMode: true,
 };
-*/
 
-const nextjsDistDir = join("src", require("./src/next.config.js").distDir);
-const nextjsServer = next({
-  dev: isDev,
-  conf: {
-    distDir: nextjsDistDir,
-    images: {
-      domains: ['hotel-backend-jrld.onrender.com'],
-    }
-  }
-});
 
-module.exports = nextjsServer;
+
+module.exports = nextConfig;
